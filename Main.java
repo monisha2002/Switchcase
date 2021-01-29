8a4ba6be8ce8ee93829c9baa3ffcee2beaf54352
monisha2002 <77716940+monisha2002@users.noreply.github.com>
2021-01-29 19:36:21 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,29 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int choice;
+        System.out.println("Pick one : 1. Hi\t2. Hey\t3. Hello\t");
+        Scanner scanner = new Scanner(System.in);
+        choice = scanner.nextInt();
+        switch (choice) {
+            case 1 :
+                System.out.println("You said Hi");
+                break;
+            case 2 :
+                System.out.println("You said Hey");
+                break;
+            case 3 :
+                System.out.println("You said Hello");
+                break;
+            default:
+                System.out.println("Invalid choice");
+
+        }
+
+    }
+}
